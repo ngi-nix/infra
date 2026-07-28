18da0b9f4b6666143c751ebca700a5b20a7e9627
Jeremy Fleischman <me@jfly.fyi>
2026-07-27 20:52:48 -0700

    Deprecate the `remotebuild` user and `root` user
    We'll eventually get rid of the `remotebuild` user in favor of giving
    everyone their own account. We'll keep the `remotebuild` group around
    and we'll add new accounts to the `remotebuild` group.
    
    We'll also disable ssh access as `root` in favor of giving admins
    individual accounts with the `wheel` group.

--- makemake/users.nix
+++ makemake/users.nix
@@ -1,7 +1,7 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 {
-  # The remotebuild user is trusted.
-  nix.settings.trusted-users = [ "remotebuild" ];
+  # Users in the remotebuild group are trusted.
+  nix.settings.trusted-users = [ "@remotebuild" ];
 
   users =
     let
@@ -27,13 +27,43 @@
     in
     {
       mutableUsers = false;
+      groups.remotebuild = { };
+
+      # TODO: drop root access, instead force people to ssh as their specific user:
+      # <https://github.com/ngi-nix/infra/issues/26>.
       users.root.openssh.authorizedKeys.keyFiles = deploy ++ infra ++ ngi;
+
+      # TODO: remove the remotebuild user once everyone is using personal accounts:
+      # <https://github.com/ngi-nix/infra/issues/26>.
       users.remotebuild = {
         isNormalUser = true;
         createHome = false;
         group = "remotebuild";
         openssh.authorizedKeys.keyFiles = infra ++ ngi ++ remotebuild;
       };
-      groups.remotebuild = { };
     };
+
+  # TODO: clean up once everyone is using personal accounts:
+  # <https://github.com/ngi-nix/infra/issues/26>.
+  systemd.tmpfiles.rules =
+    let
+      rootBashProfile = pkgs.writeText "bash_profile" /* bash */ ''
+        echo "" >&2
+        echo "*** NOTE: SSH access as the root user is deprecated, and will be removed soon! ***" >&2
+        echo "" >&2
+        echo "Please switch to to your own personal user." >&2
+        echo "See <https://github.com/ngi-nix/infra/issues/26> for details." >&2
+      '';
+      remoteBuildBashProfile = pkgs.writeText "bash_profile" /* bash */ ''
+        echo "" >&2
+        echo "*** NOTE: The remotebuild user is deprecated, and will be removed soon! ***" >&2
+        echo "" >&2
+        echo "Please switch to to your own personal user." >&2
+        echo "See <https://github.com/ngi-nix/infra/issues/26> for details." >&2
+      '';
+    in
+    [
+      "L+ /root/.bash_profile 0644 root root - ${rootBashProfile}"
+      "L+ /home/remotebuild/.bash_profile 0644 remotebuild remotebuild - ${remoteBuildBashProfile}"
+    ];
 }
