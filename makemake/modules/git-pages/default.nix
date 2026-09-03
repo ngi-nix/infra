c5aa9510444943dfa9ec76c5e55dc582bdbf3a44
phanirithvij <phanirithvij2000@gmail.com>
2026-09-03 23:04:14 +0530

    git-pages allow expiring existing sites
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- makemake/modules/git-pages/default.nix
+++ makemake/modules/git-pages/default.nix
@@ -1,31 +1,36 @@
 {
   pkgs,
   config,
-  lib,
   ...
 }:
 
 let
   # Unstable bump needed for `-expire-sites` feature
+  # TODO bump when new release is available from nixpkgs
   git-pages' = pkgs.git-pages.overrideAttrs (
     finalAttrs: previousAttrs: {
-      version = "0.9.1-unstable-2026-08-27";
+      version = "0.9.1-unstable-2026-09-03";
       src =
         (pkgs.fetchFromCodeberg {
           owner = "git-pages";
           repo = "git-pages";
-          rev = "017649481612cfe12b6127ac8cac3afdcd7ab796";
-          hash = "sha256-QUtWbPQzyiRN1wSlekoly4H8cTKvUP/egLAc+mBQOk8=";
+          rev = "507e57edbcfc0ec933a877bf26b1756ca0a61870";
+          hash = "sha256-H5Fa3zhJ17Mx6ubmkhpajXQjj1CP2XRHoegjjloe9b0=";
         })
         // {
-          tag = "0.9.1-unstable-2026-08-27";
+          tag = "0.9.1-unstable-2026-09-03";
         };
       vendorHash = "sha256-RKn3DxX/cJoR6cXkmR9UzwF9k67NZiGt9MKba178jBU=";
       ldflags = [
         "-s"
-        "-X main.versionOverride=01764948"
+        "-X main.versionOverride=507e57ed"
       ];
       doInstallCheck = false;
+      patches = (previousAttrs.patches or [ ]) ++ [
+        # NOTE: this patch was deemed a HACK not a proper fix, https://github.com/ngi-nix/infra/issues/65#issuecomment-5533062649
+        # Using this Hack while the proper fix is made.
+        ./0001-feat-add-allow-retroactive-expiration-limit-config.patch
+      ];
       meta = previousAttrs.meta // {
         changelog = "";
       };
@@ -44,6 +49,7 @@ in
         caddy = "-";
         metrics = "tcp/0.0.0.0:4002";
       };
+      settings.limits.allow-retroactive-expiration = true;
       cleanupInterval = "weekly";
 
       # NOTE: git-pages does need a password
